22f06f97a4f533f9258a5c64eed5609b3a26750d
AMMS-Lab-UTSA <amms.lab.utsa@gmail.com>
2025-05-21 12:30:07 -0500

    Update example_arch.py

--- examples/example_arch.py
+++ examples/example_arch.py
@@ -1,3 +1,14 @@
+# Distribution Statement A: 
+# Approved for Public Release; Distribution is Unlimited. PA# AFRL-2025-2361
+
+# This material is based on research sponsored by the Air Force Research Laboratory 
+# under agreement number FA8650-20-2-5853. The U.S. Government is authorized to 
+# reproduce and distribute reprints for Government purposes notwithstanding any 
+# copyright notation thereon. The views and conclusions contained herein are those 
+# of the authors and should not be interpreted as necessarily representing the 
+# official policies or endorsements, either expressed or implied, of the Air Force 
+# Research Laboratory of the U.S. Government.
+
 import sys
 import time
 import numpy as np
